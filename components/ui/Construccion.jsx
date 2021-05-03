f2e1c519fceff424974a1060ca4fcf879965206f
Cin-1 <cinthiapardos@gmail.com>
2021-05-03 15:13:55 -0300

    img

--- components/ui/Construccion.jsx
+++ components/ui/Construccion.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Suenios from "../../public/images/suenios.jpg";
 
 const Imagen = () => {
-  return <img src={Suenios}></img>;
+  return <img style={{ width: "100%" }} src={Suenios}></img>;
 };
 
 export default Imagen;
